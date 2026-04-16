e766e2842c493d0b659f9391a17faa3857138f2e
zeus-rich7 <urolboyevazimboy@gmail.com>
2026-04-16 18:05:05 +0500

    added presql file

---
+++ .sql
@@ -0,0 +1,73 @@
+INSERT INTO
+    categories(title)
+VALUES ('🥗 Salatlar');
+INSERT INTO
+    categories(title)
+VALUES ('🍕 Fast Food');
+INSERT INTO
+    categories(title)
+VALUES ('🍜 Issiq taomlar');
+
+INSERT INTO
+    foods(title, description, category_id)
+VALUES ('🥗 Sezar salati', '300 gramm tovuq filesi
+	250 gramm Aysberg salat bargi
+	150 gramm oq non bo''laklari (tost)
+	150 gramm pomidor cherry
+	150 gramm parmezan pishlog''i
+Qaylasiga:
+	100 millilitr o''simlik yog''i
+	1 ta tuxum sarig''i
+	1 choy qoshiq xantal
+	1 ta sarimsoqpiyoz tishchasi
+	2 osh qoshiq limon sharbati
+	ta''bga qarab tuz va murch', 1);
+
+
+INSERT INTO
+    foods(title, description, category_id)
+VALUES ('🥗 Olivye salati', '300 gramm tovuq filesi
+	250 gramm Aysberg salat bargi
+	150 gramm oq non bo''laklari (tost)
+	150 gramm pomidor cherry
+	150 gramm parmezan pishlog''i
+Qaylasiga:
+	100 millilitr o''simlik yog''i
+	1 ta tuxum sarig''i
+	1 choy qoshiq xantal
+	1 ta sarimsoqpiyoz tishchasi
+	2 osh qoshiq limon sharbati
+	ta''bga qarab tuz va murch', 1);
+
+
+INSERT INTO
+    foods(title, description, category_id)
+VALUES ('🍔 Burger', ' kesilgan ikki sayka yoki bulochka orasiga solingan bir yoki bir nechta kotletdan iborat sendvich. Hamburgerga solinadigan kotlet qovurilgan, dudlangan yoki olovda pishirilgan boʻlishi mumkin. Hamburgerlar koʻpincha pishloq, salat, pomidor, piyoz, tuzlangan bodring, bekon yoki chili ', 2);
+
+INSERT INTO
+    foods(title, description, category_id)
+VALUES ('🌭 Burger', 'Bulochka: Yumshoq, ko''pincha ustiga kunjut sepilgan bug''doy noni.
+Sosiska: Mol go''shti, tovuq go''shti, cho''chqa go''shti yoki ularning aralashmasidan tayyorlangan kolbasa mahsuloti.
+Souslar: Ketchup, mayonez, xantal (gorchitsa).', 2);
+
+INSERT INTO
+    foods(title, description, category_id)
+VALUES ('🍲 Sho''rva', 'Go‘sht: Qo‘y qovurg‘asi, lahm go‘sht yoki tovuq go‘shti (500g atrofida).
+Sabzavotlar: Kartoshka, sabzi, piyoz, bulg‘or qalampiri (rangli), pomidor, sholg‘om.
+Suyuqlik: Suv (masalliqlarni ko‘mib turishi kerak).
+Ziravor va ko‘katlar: Tuz, zira, qora murch, kashnich, ukrop.', 3);
+
+
+INSERT INTO
+    foods(title, description, category_id)
+VALUES ('🍛 Osh', 'Guruch: 1 kg (odatda "lazer" navi).
+Go‘sht: 1 kg (mol yoki qo‘y go‘shti).
+Sabzi: 1 kg (to‘g‘ralgan sariq va qizil sabzi aralashmasi).
+Piyoz: 2-3 dona (o‘rtacha).
+Yog‘: 200-300 gr (o‘simlik yog‘i yoki dumba).
+Ziravorlar: Zira, tuz, murch. ', 3);
+
+
+
+
+
